d67a8dbd9ccc5fd0c7c8c0e2587e883896fd803f
Stanisław Uss <stanislawuss@iMac-Stanisaw.local>
2016-01-09 15:38:43 +0100

    init

---
+++ EzFTP/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EzFTP
+//
+//  Created by Stanisław Uss on 09.01.2016.
+//  Copyright © 2016 Sl4. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ EzFTP/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EzFTP
+//
+//  Created by Stanisław Uss on 09.01.2016.
+//  Copyright © 2016 Sl4. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
